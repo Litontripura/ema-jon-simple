339f422b18df2e425662a9822be2afc3054a188b
Litontripura <litontripura01@gmail.com>
2023-03-28 11:32:44 +0600

    i have add to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import {addToDb, getShoppingCart} from '../../utilities/fakedb'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -18,7 +19,29 @@ const Shop = () => {
         // cart.push(product); 
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
+        
     }
+    useEffect(()=>{
+        const getStored = getShoppingCart()
+        const savedCart = []
+        // step 1: get id of addedProduct
+      for(const id in getStored){
+        // step 2: get product from products state using id
+        const addedProduct = products.find(product => product.id === id)
+        console.log(addedProduct);
+        if(addedProduct){
+            // step 3: add quantity
+            const quantity = getStored[id]
+            addedProduct.quantity = quantity
+            // step 4: add the addedProduct to the savedCart
+            savedCart.push(addedProduct)
+        }
+      
+      }
+    //   step 5: set The cart
+    setCart(savedCart)
+    },[products])
 
     return (
         <div className='shop-container'>
